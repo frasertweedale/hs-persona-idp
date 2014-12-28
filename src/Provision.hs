bfc109e2f0f271c61323acd3761e41322602bece
Fraser Tweedale <frase@frase.id.au>
2014-12-28 07:36:50 +0000

    deal with principal public key as JSON object
    The Persona javascript keygen routine provides the public key as a
    *serialised* JSON object, but it is expected to appear in
    certificates as a first-class object.  Since jose doesn't support
    the "DS" key format, POST the decoded JSON in the provisioning
    request and treat it as an opaque Aeson 'Value' on the server.

--- src/Provision.hs
+++ src/Provision.hs
@@ -43,7 +43,7 @@ import Crypto.Persona
 
 data ProvisioningRequest = ProvisioningRequest
   { _eml :: T.Text
-  , _pub :: T.Text    -- ^ Serialised public key
+  , _pub :: Value
   , _dur :: Integer
   }
 makeLenses ''ProvisioningRequest
